f2b3dbe7beb51a012cd3c10a72537d647fa1e568
phongbe01 <phamphong5499@gmail.com>
2019-10-16 15:45:58 +0700

    update user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(ProductSeeder::class);
-         //$this->call(UserSeederTable::class);
+         $this->call(UserSeederTable::class);
 
     }
 }
